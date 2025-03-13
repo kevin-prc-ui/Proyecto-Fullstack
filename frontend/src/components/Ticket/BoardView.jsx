491cf0ec394c3119a0ca8609a5000e837ee07545
kevin-prc-ui <nemecis_758@hotmail.com>
2025-03-13 10:22:29 -0700

    Columns updated

--- frontend/src/components/Ticket/BoardView.jsx
+++ frontend/src/components/Ticket/BoardView.jsx
@@ -4,7 +4,7 @@ import Card from "./Card";
 
 const BoardView = ({ tickets }) => {
   return (
-    <div className='w-full py-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 2xl:gap-10'>
+    <div  className='w-full py-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4 2xl:gap-10'>
       {tickets.map((ticket, index) => ( // Changed 'tickets' to 'ticket' here
         <Card ticket={ticket} key={index} />
       ))}
