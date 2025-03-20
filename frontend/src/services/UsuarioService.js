6fb5837104c8687d954186e835de10b2233d0870
kevin-prc-ui <nemecis_758@hotmail.com>
2025-03-20 16:47:21 -0700

    yes

--- frontend/src/services/UsuarioService.js
+++ frontend/src/services/UsuarioService.js
@@ -1,23 +1,36 @@
 import axios from "axios";
+import { toast } from "sonner";
 
-const REST_API_BASE_URL = "http://localhost:8080/api/users";
+const REST_API_BASE_URL = "http://localhost:8080/api"; //update the base url
+
 
 export const listUsers = () =>
   axios
-    .get(REST_API_BASE_URL+"")
+    .get(`${REST_API_BASE_URL}/users`) // Add headers to the request
     .then((response) => response)
     .catch((error) => {
-      // Detectamos específicamente errores de conexión
       if (!error.response) {
+        toast.error("Error de conexión con el servidor");
         throw new Error("Error de conexion con el servidor");
       }
       throw error;
     });
 
-export const createUser = (user) => axios.post(REST_API_BASE_URL, user);
+export const createUser = (user) => axios.post(`${REST_API_BASE_URL}/users`, user);
 
-export const getUserById = (userId) => axios.get(REST_API_BASE_URL +'/'+userId);
+export const getUserById = (userId) => axios.get(`${REST_API_BASE_URL}/users/${userId}` );
 
-export const updateUser = (userId,user) => axios.put(REST_API_BASE_URL+"/edit/"+userId, user);
+export const updateUser = (userId,user) => axios.put(`${REST_API_BASE_URL}/users/edit/${userId}`, user);
 
-export const deleteUser = (userId) => axios.delete(REST_API_BASE_URL +'/delete/'+userId);
+export const deleteUser = (userId) => axios.delete(`${REST_API_BASE_URL}/users/delete/${userId}`);
+
+export const getUserPermissions = async () => {
+  const response = await axios.get(`${REST_API_BASE_URL}/users/permisos`);
+  return response;
+};
+
+export const checkOrCreateUser = (userData) => 
+  axios.post(`${REST_API_BASE_URL}/users/check-or-create`, userData);
+
+export const login = (loginData) => axios.post(`${REST_API_BASE_URL}/auth/login`, loginData);
+export const logout = (signoutData) => axios.post(`${REST_API_BASE_URL}/auth/signout`, signoutData);
