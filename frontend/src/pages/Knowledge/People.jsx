d478d4bbbf0faf38f7189929c1536720d12fb182
kevin-prc-ui <nemecis_758@hotmail.com>
2025-04-11 16:52:42 -0700

    yes

--- frontend/src/pages/Knowledge/People.jsx
+++ frontend/src/pages/Knowledge/People.jsx
@@ -58,8 +58,8 @@ const People = () => {
   };
 
   return (
-    <div className="container mt-5">
-      <div className="row justify-content-center">
+    <div className="mt-4">
+      <div className="row p-5 pt-0 justify-content-center">
         <div className="col-md-10">
           <div className="card shadow">
             <div className="card-header bg-primary text-white">
