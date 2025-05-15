d716c8d559085042b9b52462637a47b2114c885d
Fredy16177 <alfredovb860@gmail.com>
2025-05-15 14:33:48 -0700

    SitioFormulario

--- frontend/src/pages/Knowledge/ComponentsKnow/Sites_Components/CheckGerenComponents.jsx.jsx
+++
@@ -1,9 +0,0 @@
-export const CheckGerenComponents = () => {
-
-    return (
-         <button type="button" className="list-group-item list-group-item-action">
-           Checklist Gerencial
-         </button>
-    );
-}
-export default CheckGerenComponents;
--- frontend/src/pages/Knowledge/Sites.jsx
+++ frontend/src/pages/Knowledge/Sites.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "../../styles/estilos.css";
-import CheckGerenComponents from './ComponentsKnow/Sites_Components/CheckGerenComponents.jsx';
 import MySitesComponent from './ComponentsKnow/Sites_Components/MySitesComponent.jsx';
 import SitesFinderComponent from './ComponentsKnow/Sites_Components/SitesFinderComponent.jsx';
 import CreateSitesComponent from './ComponentsKnow/Sites_Components/CreateSitesComponent.jsx';
@@ -31,7 +30,6 @@ const Sites = () => {
         <div className="row justify-content-center">
           <div className="col-md-6">
             <div className="list-group">
-              <CheckGerenComponents />
               <MySitesComponent />
               <SitesFinderComponent />
               <CreateSitesComponent />
